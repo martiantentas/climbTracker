d43e4dff41375b681de86f616555ff1b78b1b1e3
Martí Antentas <martiantentas@gmail.com>
2026-04-19 08:44:47 +0200

    translation

--- climbtracker/vite.config.ts
+++ climbtracker/vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    tailwindcss() as any,
   ],
   resolve: {
     dedupe: ['react', 'react-dom'],
